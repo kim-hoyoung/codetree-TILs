9d8d631e6c45e03589134ab218bcd2840f03ad8b
HoYoung <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-04 14:36:36 +0900

    Title: 변수 출력하기 3, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240604/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        String c = "C";
+
+        System.out.print(a + "->" + b + "->" + c);
+        // 여기에 코드를 작성해주세요.
+    }
+}
