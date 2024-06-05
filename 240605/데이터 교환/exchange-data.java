ce86775eb6d14e8fdda5be5c66adf4fbe8ad1715
HoYoung <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-05 11:46:39 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240605/데이터 교환/exchange-data.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp = 0;
+        int tempp = 0;
+
+
+        temp = c; 
+        tempp = b;
+        b = a;
+        c = tempp;
+        a = temp;
+
+
+
+        
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
