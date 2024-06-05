cbd5a52e509c4e7889cca28fdcf10400ba3e1fdc
HoYoung <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-05 11:58:39 +0900

    Title: 합을 복사하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240605/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2 , c = 3;
+        int num = 0;
+
+        num = a + b + c;
+        a = b = c = num;
+
+        System.out.print(a + " " + b + " " + c);
+        // 여기에 코드를 작성해주세요.
+    }
+}
