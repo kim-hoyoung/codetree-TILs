763068658ae377b3b10d63e6a88cd37d8bab7b7e
HoYoung <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-05 12:03:55 +0900

    Title: 점수 입출력, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println("Your score is " + n + " point.");    // 여기에 코드를 작성해주세요.
+    }
+}
