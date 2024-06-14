09902006dba2a220961ae51ee875a0a448493a22
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-14 13:56:31 +0900

    Title: 전화번호 바꾸기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240614/전화번호 바꾸기/change-phone-number.java
+++ 240614/전화번호 바꾸기/change-phone-number.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        
-        
-        
         sc.useDelimiter("-");
         int  x , y, num;
         num = sc.nextInt();
