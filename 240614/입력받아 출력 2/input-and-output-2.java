a0cda8cbdce16fce78a5fa43b9276a0f6586a678
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-14 13:43:54 +0900

    Title: 입력받아 출력 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240614/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+
+        int hnum, fnum;
+
+        hnum = sc.nextInt();
+        fnum = sc.nextInt();
+
+
+        System.out.print(hnum);
+        System.out.print(fnum);
+
+        
+    }
+}
