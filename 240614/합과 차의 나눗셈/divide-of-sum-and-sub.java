0d935e12008d9e07cf92aa1ae16e9e5b6234ad06
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-14 16:02:22 +0900

    Title: 합과 차의 나눗셈, Time: 155ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240614/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Double a, b;
+
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+
+        System.out.printf("%.2f",(a+b) / (a-b));
+
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
