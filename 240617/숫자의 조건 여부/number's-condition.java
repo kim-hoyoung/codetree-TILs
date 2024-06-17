896bfa055ebc2bde37c8f5652807f8a39bb9a100
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-17 11:02:20 +0900

    Title: 숫자의 조건 여부, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+
+        if ( a >= 113) {
+            System.out.println(1);
+        } else
+            System.out.println(0);
+        // 여기에 코드를 작성해주세요.
+    }
+}
