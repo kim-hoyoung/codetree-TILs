e400bf213568554af9ab507e059367889093e6d1
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-17 13:55:31 +0900

    Title: 살 수 있는 물건, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/살 수 있는 물건/things-able-to-buy.java
+++ 240617/살 수 있는 물건/things-able-to-buy.java
@@ -9,9 +9,9 @@ public class Main {
 
         money = sc.nextInt();
 
-        if ( money == 3000) {
+        if ( money >= 3000 ) {
             System.out.println("book");
-        } else if (money == 1000) {
+        } else if ( money >= 1000 ) {
             System.out.println("mask");
         } else
             System.out.println("no");
