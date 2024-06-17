283e38125128d3313efc1a92ddfef0068b08453d
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-17 10:08:47 +0900

    Title: 체질량지수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240617/체질량지수/bmi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int h, w;
+
+        h = sc.nextInt();
+        w = sc.nextInt();
+
+        double b = (10000*w)/(h*h);
+
+        if( b >= 25) {
+            System.out.printf("%.0f" + "\n" + "Obesity", b);
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
