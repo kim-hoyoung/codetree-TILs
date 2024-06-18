ba854222fe922bfc098e9959b055d7efc43d24bd
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-18 13:54:06 +0900

    Title: 최대 2번의 연산, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240618/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+
+        if (a%2 == 0) {
+            a = a/2;
+        }
+        
+        if (a%2 == 1) {
+            a = (a+1)/2;
+        }
+
+        System.out.println(a);
+        // 여기에 코드를 작성해주세요.
+    }
+}
