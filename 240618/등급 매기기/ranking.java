85d44997ca2c03614df5ddfe91f15d718775e0da
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-18 13:40:48 +0900

    Title: 등급 매기기, Time: 237ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240618/등급 매기기/ranking.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int score;
+
+        score = sc.nextInt();
+
+        if ( score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else
+            System.out.println("F");
+        // 여기에 코드를 작성해주세요.
+    }
+}
