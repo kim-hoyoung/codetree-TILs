e22b16d4e36251caa081b6065e8414966752bb1c
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-21 16:45:55 +0900

    Title: 두 사람, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240621/두 사람/two-person.java
+++ 240621/두 사람/two-person.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int age1 = sc.nextInt();
@@ -10,14 +10,14 @@ public class Main {
         int age2 = sc.nextInt();
         String s2 = sc.next();
 
-        if (age1 >= 19 && age2 >= 19)  {
-            if(s1.equals("M") || s2.equals("M")) {
+        if (age1 >= 19 || age2 >= 19) {
+            if (s1.equals("M") || s2.equals("M")) {
                 System.out.println(1);
-            }
-            else if (s1.equals("W") && s2.equals("W"))
+            } else if (s1.equals("W") && s2.equals("W")) {
                 System.out.println(0);
-        }else
-            System.out.println(0);
-
+            } else {
+                System.out.println(0);
+            }
+        }
     }
 }
