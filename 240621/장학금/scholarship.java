f41ab31825c55d4050fe0f9981042483151763a3
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-21 15:19:16 +0900

    Title: 장학금, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240621/장학금/scholarship.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int midleScore = sc.nextInt();
+        int lastScore = sc.nextInt();
+
+        if (midleScore >= 90) {
+            if(lastScore >= 95) {
+                System.out.println(100000);
+            } else if (lastScore >= 90){
+                System.out.println(50000);
+            } else
+            System.out.println(0);
+        }
+        System.out.println(0);
+      // 여기에 코드를 작성해주세요.
+    }
+}
