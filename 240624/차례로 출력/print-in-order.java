fb5384c259a569b48c553a1c8ff217911f277fbd
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-24 13:59:43 +0900

    Title: 차례로 출력, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

--- 240624/차례로 출력/print-in-order.java
+++ 240624/차례로 출력/print-in-order.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        int i = sc.nextInt();
-
-        for (i; i <= 17; i ++){
+        for (int i = 5; i <= 17; i ++){
             System.out.print(i + " ");
         }
-        // 여기에 코드를 작성해주세요.
     }
 }
