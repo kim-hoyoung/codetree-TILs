69a07b7a6b30711ab34e7fa4c89a8d369f11f79c
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-24 16:49:53 +0900

    Title: 감소시키며 출력하기, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240624/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 17; i >= 5; i -= 1)
+        {
+            System.out.print(i + " ");
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
