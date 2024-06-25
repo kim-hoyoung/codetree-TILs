37c0503b87e29c394e0d99da49cd7363ed1d95e0
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-25 13:51:34 +0900

    Title: a/b 출력, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240625/a/b 출력/a-divide-b.java
+++ 240625/a/b 출력/a-divide-b.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
+        System.out.print(a / b + ".");
 
-        System.out.printf("%.20f",a/b);
-        // 여기에 코드를 작성해주세요.
+        for(int i = 0; i < 20; i++) {
+            a *= 10;
+            System.out.print(a / b);
+            
+            a %= b;
+        }
     }
 }
