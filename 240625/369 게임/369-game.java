a5808de3dfb37fa9c740a6c523a514382a8978b9
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-25 17:35:24 +0900

    Title: 369 게임, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240625/369 게임/369-game.java
+++ 240625/369 게임/369-game.java
@@ -6,27 +6,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        
-
 
         
+        for (int j=1; j <= n; j ++){
+                if  (j==3 && j==6 && j==9) {
+                    System.out.print(0 + " ");
+            }   else if (j % 3 ==0 )  {
+                    System.out.print(0 + " ");
+                } 
+                else if ( j >= 30 && j <= 39) {
+                    System.out.print( 0 + " ");
+                } 
+                else if ( j >= 60 && j <= 69) {
+                    System.out.print( 0 + " ");
+                } 
+                else if (j >= 90 && j <= 99) {
+                    System.out.print ( 0 + " ");
+                } 
+                else
+                    System.out.print(j + " ");  
 
-        for(int i = 1; i <= n; i ++) {
-            if ( i % 3 ==0 )  {
-                System.out.print(0 + " ");
-
-            } else if ( i >= 30 && i <= 39) {
-                System.out.print( 0 + " ");
-            } else if ( i >= 60 && i <= 69) {
-                System.out.print( 0 + " ");
-            } else if (i >= 90 && i <= 99) {
-                System.out.print ( 0 + " ");
-            } else 
-                System.out.print(i + " ");
-                
+            }
         }
-
-
-
     }
-}
