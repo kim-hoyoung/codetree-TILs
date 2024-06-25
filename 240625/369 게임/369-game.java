4c845f71ecbe182f7ce27f5fca5c29adef1131ff
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-25 17:14:05 +0900

    Title: 369 게임, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240625/369 게임/369-game.java
+++ 240625/369 게임/369-game.java
@@ -6,13 +6,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        
+
+
+        
 
         for(int i = 1; i <= n; i ++) {
-            if ( i % 3 ==0) {
+            if ( i % 3 ==0 )  {
                 System.out.print(0 + " ");
 
+            } else if ( i >= 30 && i <= 39) {
+                System.out.print( 0 + " ");
+            } else if ( i >= 60 && i <= 69) {
+                System.out.print( 0 + " ");
+            } else if (i >= 90 && i <= 99) {
+                System.out.print ( 0 + " ");
             } else 
                 System.out.print(i + " ");
+                
         }
 
 
