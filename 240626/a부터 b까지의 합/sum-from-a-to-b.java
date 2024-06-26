fb56f0b60b9359b9222307a83b90d33c18f811d1
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-26 12:19:14 +0900

    Title: a부터 b까지의 합, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240626/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sumVal = 0;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    
+
+        for (int i = a; i <= b; i++) {
+            sumVal += i;
+        }
+        System.out.print(sumVal);
+    }
+}
