66260dbada248e4c19b5105f549e5b2e9c9bf85e
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-26 11:59:59 +0900

    Title: 윤년 횟수 구하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240626/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int leapYear = 0;
+        int commonYear =0;
+
+        for(int i = 1; i < n; i ++) {
+             
+            if (i%100==0 && i%400 != 0) {
+                commonYear++; 
+            }
+            else if(i%4 == 0) {
+                leapYear++;
+            }
+        }
+        System.out.print(leapYear);
+       
+    }
+}
