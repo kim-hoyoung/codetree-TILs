a163e5267bf65ee04def06ec7f93df11378d5ae6
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-27 10:38:13 +0900

    Title: 1부터 곱셈하기, Time: 108ms, Memory: 10MB, Status: Passed - CodeTree

--- 240627/1부터 곱셈하기/multiple-from-one.java
+++ 240627/1부터 곱셈하기/multiple-from-one.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (int i =1; i <= 10; i++) {
             prod *= i;
-            if (prod > n) {
+            if (prod >= n) {
                 System.out.print(i);
                 break;
             }
