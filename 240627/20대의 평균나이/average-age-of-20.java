2ee07f463d9dd3324ccb49f06230dfc00d8a2569
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-27 14:11:11 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+
+        while(true) {
+            int age = sc.nextInt();
+            if(age > 29) {
+                break;
+            }
+            else
+                count++;
+            sum += age;
+
+        }
+        System.out.printf("%.2f",(double)sum/count);
+
+
+
+    }
+}
