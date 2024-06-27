e2e9b73354c48fddd82daf36b1e75b0e1421d8de
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-27 10:49:40 +0900

    Title: 1까지 나누기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240627/1까지 나누기/divide-by-1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+        int result = 0;
+
+        for(int i = 1; i < 100; i++ ) {
+            if(n/i != 0) {
+                n = n/i;
+                count++;
+                continue;
+            }
+            if (n/i == 0) {
+                count++;
+                System.out.println(count);
+                break;
+            }
+        }
+    }
+}
