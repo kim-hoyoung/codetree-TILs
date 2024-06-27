6ce6ac6a1f2e9d324bd8fe62603aadeee1fb4576
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-27 10:50:13 +0900

    Title: 1까지 나누기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240627/1까지 나누기/divide-by-1.java
+++ 240627/1까지 나누기/divide-by-1.java
@@ -13,7 +13,7 @@ public class Main {
                 count++;
                 continue;
             }
-            if (n/i == 0) {
+            if (n/i == 1) {
                 count++;
                 System.out.println(count);
                 break;
