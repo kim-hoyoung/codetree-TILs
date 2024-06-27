32b8a35b89037540bdcec16a8da1eac7be353d94
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-27 22:58:57 +0900

    Title: 별표 출력하기 5, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240627/별표 출력하기 5/print-star-5.java
+++ 240627/별표 출력하기 5/print-star-5.java
@@ -2,42 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
+        int z = 0;
+        for (int j = 0; j < n; j++) {
 
-        for (int a = 0; a < n; a++) {
-            for (int b = 0; b < n; b++) {
-                System.out.print("*");
+            for (int i = 0; i < (n-z); i++) {
+                for(int c = n-z; c > 0 ; c--){
+                    System.out.print("*");
+                }
+                System.out.print(" ");
             }
-            System.out.print(" ");
-
-        }
-        System.out.println();
-
-        for (int c = 0; c < n-1; c++) {
-            for (int d = 0; d < n-1; d++) {
-                System.out.print("*");
-            }
-            System.out.print(" ");
-            
-        } System.out.println();
-        
-        for (int e = 0; e < n-2; e++) {
-
-            for (int f = 0; f < n-2; f++) {
-                System.out.print("*");
-            }
-            System.out.print(" ");
-        }
-        System.out.println();
-        
-        for (int g = 0; g < n-3; g++) {
-            
-            for (int h = 0; h < n-3; h++) {
-                System.out.print("*");
+            z++;
+            System.out.println();
             }
         }
-
     }
-}
