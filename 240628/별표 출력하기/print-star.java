3f523911c46344058eeae041c5150d833b09f37b
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-06-28 12:26:03 +0900

    Title: 별표 출력하기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240628/별표 출력하기/print-star.java
+++ 240628/별표 출력하기/print-star.java
@@ -5,15 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= n - i; j++) {
-                System.out.print("*");
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
