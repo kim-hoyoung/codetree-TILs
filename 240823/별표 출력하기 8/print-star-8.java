40a959fd3573dd0eef899d5dfed9d1eca2d1157c
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-08-23 11:50:30 +0900

    Title: 별표 출력하기 8, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240823/별표 출력하기 8/print-star-8.java
+++ 240823/별표 출력하기 8/print-star-8.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 6; i++) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
             if (i%2 !=0) {
                 for(int j=0; j <i+1; j++) {
                     System.out.print("* ");
