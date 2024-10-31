882015915a94167578b8d9208452ca19f3d16048
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-10-31 16:46:11 +0900

    Title: 열 단위로 반복하여 출력, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241031/열 단위로 반복하여 출력/print-in-column.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+
+                System.out.print(i+1);
+            }
+            System.out.println(
+
+            );
+        }
+    }
+}
