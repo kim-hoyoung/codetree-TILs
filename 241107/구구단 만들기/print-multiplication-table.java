2c6a0df25e7ad9e9216e04b656364b547a703fb6
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-11-07 18:33:23 +0900

    Title: 구구단 만들기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241107/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int min = sc.nextInt();
+        int max = sc.nextInt();
+
+        int count = max/min;
+        int fixNum = max;
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < count+1; j++) {
+                System.out.print(fixNum + " * " + i + " = " + (fixNum*i));
+                if (j < count )
+                    System.out.print(" / ");
+                if (fixNum != min) {
+                    fixNum = fixNum-2;
+                }
+            }
+            fixNum = max;
+            System.out.println();
+        }
+    }
+}
