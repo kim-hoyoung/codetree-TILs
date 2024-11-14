455f0daeee3756542da4e2b33af68a10c3919f7a
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-11-15 00:36:09 +0900

    Title: 일의 자리 배열, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241115/일의 자리 배열/array-with-units.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int[] arr = new int[10];
+            int sum = 0;
+
+
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+
+            System.out.print(arr[0] + " " + arr[1] + " " );
+
+            for ( int i = 2; i < 10; i ++) {
+                arr[i] = arr[i-2] + arr[i-1];
+                sum += arr[i];
+
+                if (sum > 10) {
+                    sum = sum%10;
+                    System.out.print(sum + " ");
+                } else
+
+                System.out.print(sum + " ");
+
+                sum = 0;
+
+            }
+
+
+
+        }
+    }
