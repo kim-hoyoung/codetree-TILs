13b457ba3af8d7f3f634c2255a5cebd7a70805c0
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-11-26 23:55:06 +0900

    Title: 100 도달하기, Time: 137ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241126/100 도달하기/reach-100.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[10];
+        int count = 0;
+
+
+        for(int i = 0; i < 100; i++) {
+            if(i==0) {
+                arr[count] = 1;
+                System.out.print(arr[count] + " ");
+            } else if(i==1) {
+                arr[1] = n;
+                count++;
+                System.out.print(arr[count] + " ");
+
+            } else if (i>1) {
+                arr[i] = arr[count]+arr[count-1];
+                System.out.print(arr[i] + " ");
+                count++;
+                if(arr[count]>100){
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
