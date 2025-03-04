fe95f1f80fd2ac54686b21a1e0b76c87275813d0
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-04 15:49:25 +0900

    Title: 달에서 무게 구하기, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 250304/달에서 무게 구하기/weight-on-the-moon.js
+++ 250304/달에서 무게 구하기/weight-on-the-moon.js
@@ -1,4 +1,4 @@
 let weight = 13;
 let gravity = 0.165;
 
-console.log(weight + " * " + gravity.toFixed(3) + " = " + weight*gravity.toFixed(3));
+console.log(weight + " * " + gravity.toFixed(6) + " = " + (weight*gravity).toFixed(6));
