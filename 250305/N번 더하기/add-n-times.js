3f08f7c465b080a005d4d1342ad0027985c2eaa1
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-05 16:45:37 +0900

    Title: N번 더하기, Time: 198ms, Memory: 15MB, Status: Passed - CodeTree

--- 250305/N번 더하기/add-n-times.js
+++ 250305/N번 더하기/add-n-times.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-let input = fs.readFileSync(0).trim().toString().split(" ");
+let input = fs.readFileSync(0).toString().trim().split(" ");
 
 let A = Number(input[0]);
 let N = Number(input[1]);
 
-let sum = A + N
+let sum = A + N;
 
 for(let i = 0; i < N; i++) {
     console.log(sum);
