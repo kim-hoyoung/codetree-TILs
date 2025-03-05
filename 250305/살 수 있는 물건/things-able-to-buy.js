614683fba399621aee89e32a7bb039897d9d0825
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-05 16:22:24 +0900

    Title: 살 수 있는 물건, Time: 175ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250305/살 수 있는 물건/things-able-to-buy.js
+++ 250305/살 수 있는 물건/things-able-to-buy.js
@@ -4,9 +4,9 @@ let input = fs.readFileSync(0).toString().trim();
 
 let n = Number(input);
 
-if (n >= 1000) {
+if (n >= 3000) {
     console.log("mask");
-} else if (  n>= 3000) {
+} else if (  n>= 1000) {
     console.log("book");
 } else {
     console.log("no");
