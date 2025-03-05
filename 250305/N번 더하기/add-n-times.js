5c9b22a166c152aa61ebbb5a187b0f8cefae7a40
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-05 16:44:54 +0900

    Title: N번 더하기, Time: 172ms, Memory: 14MB, Status: Runtime Error - CodeTree

--- 250305/N번 더하기/add-n-times.js
+++ 250305/N번 더하기/add-n-times.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-let input = readFileSync(0).trim().toString().split(" ");
+let input = fs.readFileSync(0).trim().toString().split(" ");
 
 let A = Number(input[0]);
 let N = Number(input[1]);
