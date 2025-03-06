fb7a68ab9e8a26f788d0d33ef655c6bf3f365e60
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-06 14:51:06 +0900

    Title: 점수 비교, Time: 233ms, Memory: 15MB, Status: Passed - CodeTree

--- 250306/점수 비교/score-comparison.js
+++ 250306/점수 비교/score-comparison.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 
-let input = fs.readFileSync(0).toString().trim().split(" ");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let A = Number(input[0]);
-let B = Number(input[1]);
+arrA = input[0].split(" ");
+arrB = input[1].split(" ");
 
-let result = "";
 
-for(let i = B; i >= A; i--) {
-    result += i + " ";
+let mathA = Number(arrA[0]), engA = Number(arrA[1]);
+let mathB = Number(arrB[0]), engB = Number(arrB[1]);
+
+if ( mathA > mathB && engA > engB ) {
+    console.log(1);
+} else {
+    console.log(0);
 }
 
-console.log(result.trim());
